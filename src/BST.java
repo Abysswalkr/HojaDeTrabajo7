1b7a924782131f1d0a1261c6dd3dfe35a1767c69
Jose Gramajo <gra22907@uvg.edu.gt>
2023-06-02 15:12:28 -0600

    Metod of Binary search tree implemented with nodes

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,63 @@
+//Codigo Obtenido de la Pagina Geeks for Geeks
+class BST {
+    Node root;
+
+    public BST()
+    {
+        root = null;
+    }
+
+    public void insert(int Key, Association<Integer, String[]> Values)
+    {
+        root = insertRec(root, Key, Values);
+    }
+
+    public Node insertRec(Node Root, int Key, Association<Integer, String[]> Values)
+    {
+
+        if (Root == null)
+        {
+            Root = new Node(Key, Values);
+            return Root;
+        }
+
+        if (Key < Root.key)
+            Root.left = insertRec(Root.left, Key, Values);
+        else if (Key > Root.key)
+            Root.right = insertRec(Root.right, Key, Values);
+
+        return Root;
+    }
+
+    public void inorder()
+    {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node Root)
+    {
+        if (Root != null) {
+            inorderRec(Root.left);
+            System.out.println(Root.key);
+            inorderRec(Root.right);
+        }
+    }
+
+    public Node Search(Node Root, int Key)
+    {
+        if (Root ==null || Root.key== Key)
+            return Root;
+
+        if (Root.key < Key)
+            return Search(Root.right, Key);
+
+        return Search(Root.left, Key);
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
 
 
 
--- src/Node.java
+++ src/Node.java
@@ -1,1 +1,22 @@
+public class Node {
+    Association<Integer, String[]> Traductions;
+    int key;
+    Node left, right;
+
+    public Node(int item, Association<Integer, String[]> Data)
+    {
+        key = item;
+        left = right = null;
+        Traductions = Data;
+    }
+
+    public String[] getTraduccion() {
+        return Traductions.getValue();
+    }
+
+    public Association<Integer, String[]> getTraductions() {
+        return Traductions;
+    }
+
+}
 
